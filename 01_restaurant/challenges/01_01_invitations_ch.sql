2444fcd7ea18f02ca0b3da30b6c060b07128137e
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:23:59 +0530

    Update 01_01_invitations_ch.sql

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,9 @@
 -- Generate a list of customer information.
+SELECT * FROM Customers;
 
 -- Show their first name, last name, and email address.
+SELECT FirstName,LastName,Email
+FROM Customers;
 -- Sort the list of results by last name.
+SELECT * FROM Customers 
+ORDER BY LastName;
