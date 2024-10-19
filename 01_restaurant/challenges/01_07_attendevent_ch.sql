79881995aa3cf77608a1f8bb476cdec6a710b0a4
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:26:11 +0530

    Update 01_07_attendevent_ch.sql

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -1,4 +1,11 @@
 -- Register a customer for our Anniversary event.
+SELECT * 
+FROM Customers 
+WHERE Email='atapley2j@kinetecoinc.com';
+
+INSERT INTO Special_People VALUES
+(92,3);
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+SELECT * FROM Special_People;
