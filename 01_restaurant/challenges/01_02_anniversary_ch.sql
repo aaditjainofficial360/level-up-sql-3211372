7546094f7892c435404cc694c07485c909c39750
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:24:22 +0530

    Update 01_02_anniversary_ch.sql

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -1,6 +1,11 @@
 -- Create a table in the database to store customer
 -- responses to our anniversary invitation.
 
+CREATE TABLE Special_People(
+Customer ID INT NOT NULL,
+SIZE INT
+);
+
 -- Associate a customer's ID number with the number of people
 -- they plan to bring in their party.
 
