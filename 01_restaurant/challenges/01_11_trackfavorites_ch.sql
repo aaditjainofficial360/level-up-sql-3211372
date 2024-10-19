741db57e6b4d180386e8a463b1688a0aee74ca83
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 18:17:34 +0530

    Update 01_11_trackfavorites_ch.sql

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -1,4 +1,10 @@
 -- Update information in the database.
+UPDATE Customers
+SET FavoriteDish = 
+(SELECT DishID FROM Dishes WHERE Name='Quinoa Salmon Salad')
+WHERE
+FirstName='Cleo';
+
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
