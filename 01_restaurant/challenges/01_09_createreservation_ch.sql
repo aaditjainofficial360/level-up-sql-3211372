0bbc004232b1dfe090301f222179c2325d51409d
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:27:11 +0530

    Update 01_09_createreservation_ch.sql

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -1,6 +1,20 @@
 -- Create a reservation for a customer who may or may not
 -- already be listed in our Customers table.
 
+INSERT INTO 
+Customers(FirstName,LastName,Email,Phone) VALUES
+('Sam','McAdams','smac@kinetecoinc.com','5555551212');
+
+INSERT INTO Reservations(CustomerID,Date,PartySize) 
+VALUES
+((SELECT CustomerID FROM Customers
+WHERE FirstName='Sam'),'2022-08-12 18:00:00',5);
+
+SELECT * FROM Reservations 
+WHERE CustomerID = 
+(SELECT CustomerID FROM Customers
+WHERE FirstName='Sam');
+
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
