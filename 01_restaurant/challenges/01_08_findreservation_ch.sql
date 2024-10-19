e3039bd6ad9e55d9063ecb63470a6437fbae371a
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:26:43 +0530

    Update 01_08_findreservation_ch.sql

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -1,6 +1,10 @@
 -- Find the reservation information for a customer whose
 -- name we aren't quite sure how to spell.
-
+SELECT * 
+FROM Reservations
+WHERE CustomerID IN
+(SELECT CustomerID FROM Customers WHERE LastName LIKE 'St%')
+AND Date>'2022-06-13';
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
