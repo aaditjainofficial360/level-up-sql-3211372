20c622dfc8dc45cd163821072e597d3f499e19cc
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 18:06:01 +0530

    Update 01_10_takeorder_ch.sql

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -1,6 +1,53 @@
 -- Enter a customer's delivery order into our database, 
 -- and provide the total cost of the items ordered.
 
+-- Generating the OrderID
+INSERT INTO Orders(CustomerID,OrderDate) VALUES
+((SELECT CustomerID
+FROM Customers 
+WHERE FirstName='Loretta' 
+AND LastName='Hundey'),'2022-07-20 14:00:00');
+
+-- Making an entry to OrderDishes Table for each Dish entry
+INSERT INTO OrdersDishes(OrderID,DishID)
+VALUES 
+((SELECT OrderID FROM Orders 
+WHERE CustomerID =
+(SELECT CustomerID FROM Customers WHERE FirstName='Loretta' 
+ AND LastName ='Hundey' )),
+ (SELECT DishID FROM Dishes WHERE Name = 'House Salad'));
+
+INSERT INTO OrdersDishes(OrderID,DishID)
+VALUES 
+((SELECT OrderID FROM Orders 
+WHERE CustomerID =
+(SELECT CustomerID FROM Customers WHERE FirstName='Loretta' 
+ AND LastName ='Hundey' )),
+ (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+INSERT INTO OrdersDishes(OrderID,DishID)
+VALUES 
+((SELECT OrderID FROM Orders 
+WHERE CustomerID =
+(SELECT CustomerID FROM Customers WHERE FirstName='Loretta' 
+ AND LastName ='Hundey' )),
+ (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers'));
+
+-- Bill Calculation after placing the orders
+SELECT Orders.OrderID,FirstName,LastName,SUM(Price*1)
+FROM Customers
+JOIN Orders
+ON Orders.CustomerID=Customers.CustomerID
+JOIN OrdersDishes
+ON Orders.OrderID=OrdersDishes.OrderID
+JOIN Dishes
+ON OrdersDishes.DishID=Dishes.DishID
+WHERE OrderDate = '2022-07-20 14:00:00' AND
+FirstName ='Loretta' AND LastName ='Hundey' AND 
+OrdersDishes.DishID IN 
+(SELECT DishID FROM Dishes WHERE Name IN 
+('House Salad','Mini Cheeseburgers','Tropical Blue Smoothie'))
+GROUP BY Orders.OrderID;
 -- Use this order information:
 -- Customer: Loretta Hundey, at 6939 Elka Place
 -- Items: 1 House Salad, 1 Mini Cheeseburgers, and
