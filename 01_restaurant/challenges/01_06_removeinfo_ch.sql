ae91be86d2986231614deba03eda8820a48bf5f3
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 17:25:48 +0530

    Update 01_06_removeinfo_ch.sql

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -1,5 +1,10 @@
 -- Remove incorrect information from the database.
+SELECT CustomerID FROM Customers WHERE FirstName='Norby';
 
+DELETE
+FROM Reservations
+WHERE CustomerID = 64
+AND DATE>'2022-07-21';
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
