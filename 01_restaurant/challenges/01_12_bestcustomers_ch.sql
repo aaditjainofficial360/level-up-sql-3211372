f692a1e6a108e15efb2726738ce1b13c82b510c8
Aadit Jain <105768739+aaditjainofficial360@users.noreply.github.com>
2024-10-19 18:53:51 +0530

    Update 01_12_bestcustomers_ch.sql

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,11 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT Orders.CustomerID,FirstName,LastName,COUNT(OrderID) AS "ORDERS COUNT"
+FROM Orders
+JOIN Customers
+ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Orders.CustomerID
+ORDER BY 4 DESC
+LIMIT 10;
